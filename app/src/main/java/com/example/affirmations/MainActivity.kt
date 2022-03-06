49f8686809aecccaa71e3f6cd9df9646d7cb916f
goarnaiz <goarnaiz@unirioja.es>
2022-03-06 19:43:07 +0100

    Cómo usar RecyclerView para obtener una lista desplazable

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.data.DataSource
+import com.example.affirmations.adapter.ItemAdapter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
