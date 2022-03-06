fe9c4c429b022b1855350b3d8c0fe79a5765f3c4
goarnaiz <72554806+goarnaiz@users.noreply.github.com>
2022-03-06 20:34:53 +0100

    Cómo crear listas desplazables con imágenes

--- app/src/main/java/com/example/affirmations/data/DataSource.kt
+++ app/src/main/java/com/example/affirmations/data/DataSource.kt
@@ -9,11 +9,11 @@ class DataSource {
 
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.ic_launcher_background),
-            Affirmation(R.string.affirmation2, R.drawable.image2ic_launcher_foreground),
-            Affirmation(R.string.affirmation3, R.drawable.image2ic_launcher_background),
-            Affirmation(R.string.affirmation4, R.drawable.image2ic_launcher_foreground),
-            Affirmation(R.string.affirmation5, R.drawable.image5ic_launcher_background),
-            Affirmation(R.string.affirmation6, R.drawable.image6ic_launcher_foreground),
+            Affirmation(R.string.affirmation2, R.drawable.ic_launcher_foreground),
+            Affirmation(R.string.affirmation3, R.drawable.ic_launcher_background),
+            Affirmation(R.string.affirmation4, R.drawable.ic_launcher_foreground),
+            Affirmation(R.string.affirmation5, R.drawable.ic_launcher_background),
+            Affirmation(R.string.affirmation6, R.drawable.ic_launcher_foreground),
             Affirmation(R.string.affirmation7, R.drawable.ic_launcher_background)
         )
     }
